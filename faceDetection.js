7adf0da03ed927a0ecf982b84d52aadd4f88e285
Jai <jplko.simt@gmail.com>
2016-11-11 17:59:34 +0530

    JP/Raju | Getting faceData in given time interval is done.

--- faceDetection.js
+++ faceDetection.js
@@ -2,8 +2,6 @@
 var video = document.getElementById('video');
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
-// var SecondaryCanvas  = document.getElementById("dtemp");
-// var SecondaryCanvas_Context = SecondaryCanvas.getContext ("2d");
 var tracker = new tracking.ObjectTracker('face');
 
 tracker.setInitialScale(4);
@@ -12,8 +10,20 @@ tracker.setEdgesDensity(0.1);
 tracking.track('#video', tracker, { camera: true });
 
 var faces = [];
+var faceData;
+var timeIntervalId;
+var getImageFrame = function(){
+	if(faceData){
+		faces.push(faceData);
+	}
+	if (faces.length == 3){
+		clearInterval(timeIntervalId);
+	}
+};
 
 tracker.on('track', function(event) {
+	if(!timeIntervalId)
+		timeIntervalId = setInterval(getImageFrame, 3000);
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	event.data.forEach(function(rect) {
     	context.drawImage(video, 320, 240);
@@ -23,5 +33,6 @@ tracker.on('track', function(event) {
 		context.fillStyle = "#fff";
 		context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
 		context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+		faceData = context.getImageData(rect.x, rect.y, rect.width, rect.height);
     });
 });
